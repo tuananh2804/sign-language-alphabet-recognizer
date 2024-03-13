10c93538829f2e77dce8f2fb744bab92906e7fc3
loochuynhh <lochuynhhsb@gmail.com>
2024-03-13 13:01:36 +0700

    loc first commit

--- classify_webcam.py
+++ classify_webcam.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import cv2
-
+import urllib.request
 # Disable tensorflow compilation warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow.compat.v1 as tf
@@ -45,55 +45,61 @@ with tf.Session() as sess:
 
     c = 0
 
-    cap = cv2.VideoCapture(0)
-
+    # cap = cv2.VideoCapture('rtsp://192.168.1.18:81/stream')
+    url = 'http://192.168.1.18/cam-hi.jpg'
     res, score = '', 0.0
     i = 0
     mem = ''
     consecutive = 0
     sequence = ''
-    
     while True:
-        ret, img = cap.read()
-        img = cv2.flip(img, 1)
         
-        if ret:
-            x1, y1, x2, y2 = 100, 100, 300, 300
-            img_cropped = img[y1:y2, x1:x2]
+        imgResp = urllib.request.urlopen(url)
+        imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+        img = cv2.imdecode(imgNp,-1)
+        # img = cap.read()
+        img = cv2.flip(img, 1)
+        # 
+        
+        x1, y1, x2, y2 = 100, 100, 300, 300
+        img_cropped = img[y1:y2, x1:x2]
 
-            c += 1
+        c += 1
+        try:
             image_data = cv2.imencode('.jpg', img_cropped)[1].tostring()
-            
-            a = cv2.waitKey(1) # waits to see if `esc` is pressed
-            
-            if i == 4:
-                res_tmp, score = predict(image_data)
-                res = res_tmp
-                i = 0
-                if mem == res:
-                    consecutive += 1
+        except:
+            continue
+        
+        a = cv2.waitKey(1) # waits to see if `esc` is pressed
+        
+        if i == 4:
+            res_tmp, score = predict(image_data)
+            res = res_tmp
+            i = 0
+            if mem == res:
+                consecutive += 1
+            else:
+                consecutive = 0
+            if consecutive == 2 and res not in ['nothing']:
+                if res == 'space':
+                    sequence += ' '
+                elif res == 'del':
+                    sequence = sequence[:-1]
                 else:
-                    consecutive = 0
-                if consecutive == 2 and res not in ['nothing']:
-                    if res == 'space':
-                        sequence += ' '
-                    elif res == 'del':
-                        sequence = sequence[:-1]
-                    else:
-                        sequence += res
-                    consecutive = 0
-            i += 1
-            cv2.putText(img, '%s' % (res.upper()), (100,400), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 4)
-            cv2.putText(img, '(score = %.5f)' % (float(score)), (100,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
-            mem = res
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
-            cv2.imshow("img", img)
-            img_sequence = np.zeros((200,1200,3), np.uint8)
-            cv2.putText(img_sequence, '%s' % (sequence.upper()), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-            cv2.imshow('sequence', img_sequence)
-            
-            if a == 27: # when `esc` is pressed
-                break
+                    sequence += res
+                consecutive = 0
+        i += 1
+        cv2.putText(img, '%s' % (res.upper()), (100,400), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 4)
+        cv2.putText(img, '(score = %.5f)' % (float(score)), (100,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+        mem = res
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
+        cv2.imshow("img", img)
+        img_sequence = np.zeros((200,1200,3), np.uint8)
+        cv2.putText(img_sequence, '%s' % (sequence.upper()), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv2.imshow('sequence', img_sequence)
+        
+        if a == 27: # when `esc` is pressed
+            break
 
 # Following line should... <-- This should work fine now
 cv2.destroyAllWindows() 
